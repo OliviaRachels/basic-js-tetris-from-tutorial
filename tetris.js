80e386b06dc5f0b9f814e59c3f8cfb89182cd1f1
Olivia <86005404+OliviaRachels@users.noreply.github.com>
2021-07-27 17:47:11 -0400

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -68,18 +68,18 @@ document.addEventListener('DOMContentLoaded', () => {
     //make the tetromino move down every second
     timerId = setInterval(moveDown, 1000)
 
-//assign functions to keycodes
-    function control(e) {
-        if(e.keyCode === 37) {
-            moveLeft()
-        } else if (e.keycode === 38) {
-            rotate()
-        } else if (e.keycode === 39) {
-            moveRight()
-        } else if (e.keycode === 40) {
-            moveDown()
-        }
+  //assign functions to keyCodes
+  function control(e) {
+    if(e.keyCode === 37) {
+      moveLeft()
+    } else if (e.keyCode === 38) {
+      rotate()
+    } else if (e.keyCode === 39) {
+      moveRight()
+    } else if (e.keyCode === 40) {
+      moveDown()
     }
+  }
   document.addEventListener('keyup', control)
 
     //move down funciton
@@ -102,8 +102,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-   //move the tetromino left, unless is at the edge or there is a blockage
-   function moveLeft() {
+  //move the tetromino left, unless is at the edge or there is a blockage
+  function moveLeft() {
     undraw()
     const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
     if(!isAtLeftEdge) currentPosition -=1
@@ -113,16 +113,16 @@ document.addEventListener('DOMContentLoaded', () => {
     draw()
   }
 
-  //move the tetromino left, unless is at the edge or there is a blockage
+  //move the tetromino right, unless is at the edge or there is a blockage
   function moveRight() {
-      undraw()
-      const isAtRightEdge = current.some(index => (currentPosition + index) % width === -1)
-      if(!isAtRightEdge) currentPosition +=1
-      if(current.some(index =>squares[currentPosition + index].classList.contains('taken'))) {
-        currentPosition -=1
-      }
-      draw()
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+    if(!isAtRightEdge) currentPosition +=1
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -=1
     }
+    draw()
+  }
 
 
 })
